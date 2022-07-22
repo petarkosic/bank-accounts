dd51e907279b888105f7d12aa25140764ce98d65
petarkosic <avt3914@gmail.com>
2022-07-22 23:50:11 +0200

    ADD - db connection, setup cors, log

--- server/app.js
+++ server/app.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+import clientsRoute from './routes/clientsRoute.js';
+import moneyTransferRoute from './routes/moneyTransferRoute.js';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World!' })
 })
+
+app.use('/clients', clientsRoute);
+app.use('/money-transfer', moneyTransferRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);

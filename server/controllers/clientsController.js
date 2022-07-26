0a8bef7b1fdc8eb8d095667c6381166264bdd027
petarkosic <avt3914@gmail.com>
2022-07-26 23:37:36 +0200

    ADD - get all clients query, get one client query

--- server/controllers/clientsController.js
+++ server/controllers/clientsController.js
@@ -4,7 +4,7 @@ export const getAllClients = async (req, res, next) => {
 
     try {
         let query = `
-        SELECT clients.client_id, first_name, last_name, date_of_birth, list_of_accounts, country_name, country_code, street_name, house_number, postal_code, type_of_customer FROM clients LEFT JOIN client_address ON clients.client_id = client_address.client_id LEFT JOIN client_accounts_limit ON clients.client_id = client_accounts_limit.client_id; 
+        SELECT client_id, first_name, last_name, date_of_birth, list_of_accounts FROM clients;
         `;
 
         let clients = await pool.query(query);
@@ -21,7 +21,11 @@ export const getClient = async (req, res, next) => {
     const id = req.params.id;
 
     try {
-        let client = await pool.query('SELECT * FROM clients WHERE client_id = $1', [id]);
+        let query = `
+        SELECT clients.client_id, first_name, last_name, date_of_birth, list_of_accounts, country_name, country_code, street_name, house_number, postal_code, type_of_customer, type_of_account, credit_payment, card_limit, withdrawal_fee, accounts.account_id, account_number, currency_name, currency_code, deposited_amount FROM clients LEFT JOIN client_address ON clients.client_id = client_address.client_id LEFT JOIN accounts ON accounts.client_id = clients.client_id LEFT JOIN accounts_limit ON accounts.account_id = accounts_limit.account_id WHERE clients.client_id = $1;
+        `;
+
+        let client = await pool.query(query, [id]);
 
         res.status(200).json({
             client: client.rows,

1bdfd9be769e384460058fa8db6c445c6583fba4
petarkosic <avt3914@gmail.com>
2022-08-01 23:10:00 +0200

    ADD - client card

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -1,9 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchClient } from './../hooks/fetchClients';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function Client() {
     const { clientId } = useParams();
+    const navigate = useNavigate();
 
     const { data, error, isError, isLoading } = useQuery(['client', clientId], () => fetchClient(clientId));
 
@@ -15,15 +16,39 @@ function Client() {
     }
 
     return (
-        <div>
-            {data?.map(cl => (
-                <div key={cl.client_id}>
-                    <h1>{cl.first_name}</h1>
-                    <h2>{cl.last_name}</h2>
+        <div className='client'>
+            <button onClick={() => navigate(-1)}>Go Back</button>
+            {data?.map(client => (
+                <div key={client.client_id} className='client-card'>
+                    <div className='client-info'>
+                        <div className="client-fullname">
+                            <h1>{client.first_name}{' '}{client.last_name}</h1>
+                            <span className={client.type_of_customer === 'premium' ? 'premium' : 'regular'}></span>
+                        </div>
+                        <p className='account-number'>{client.account_number}</p>
+                        <div className="type">
+                            <p>{client.type_of_account}</p>
+                            <p>{client.type_of_customer}</p>
+                        </div>
+                        <div className="address">
+                            <p>{client.street_name}</p>
+                            <p>{client.house_number}</p>
+                            <p>{client.postal_code}</p>
+                            <p>{client.country_name}</p>
+                            <p>({client.country_code})</p>
+                        </div>
+                    </div>
+                    <div className="client-money">
+                        <p>Card limit: {client.card_limit}</p>
+                        <p>Credit payment: {client.credit_payment}</p>
+                        <p>Deposited amount: {client.deposited_amount.slice(0, -2)}</p>
+                        <p>Currency name: {client.currency_name}</p>
+                        <p>Currency code: {client.currency_code}</p>
+                    </div>
                 </div>
             ))
             }
-        </div >
+        </div>
     );
 }
 

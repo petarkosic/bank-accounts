7fd23ce948ff3dc2643e8edb9fa0d362042a15bb
petarkosic <avt3914@gmail.com>
2022-08-02 22:54:49 +0200

    ADD - header search bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const Header = () => {
+    const location = useLocation();
+
     return (
         <div className="header" >
             <h1 className='title'>
                 <Link to={'/'}>BA</Link>
             </h1>
+            {location.pathname === '/' ?
+                <>
+                    <input
+                        type="text"
+                        placeholder="Search by account number"
+                        className="search"
+                    />
+                    <button className="client-add">Add Client</button>
+                </>
+                :
+                <button className="client-edit">Edit info</button>
+            }
         </div>
     )
 }

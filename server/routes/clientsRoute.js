1b4b309c9816115c3f132eae71730114c0eb8448
petarkosic <avt3914@gmail.com>
2022-08-04 23:50:54 +0200

    ADD - added change address server route

--- server/routes/clientsRoute.js
+++ server/routes/clientsRoute.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getAllClients, getClient } from './../controllers/clientsController.js';
+import { getAllClients, getClient, changeAddress } from './../controllers/clientsController.js';
 
 const router = Router();
 
 router.get('/', getAllClients);
 router.get('/:id', getClient);
+router.post('/change-address/:id', changeAddress);
 
 export default router;

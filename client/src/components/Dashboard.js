d0edeedef176ba4607b603a580fd750e91409102
petarkosic <avt3914@gmail.com>
2022-09-30 20:53:59 +0200

    ADD - client side, style dashboard button

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+// TODO: show number of VIP customers grouped by country
+// TODO: show customers that are about to reach credit card limit (10% buffer left)
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <div className="dashboard-wrapper">
+            <div>Number of premium customers by country</div>
+            <div>Customers about to reach credit card limit</div>
+        </div>
     )
 }
 

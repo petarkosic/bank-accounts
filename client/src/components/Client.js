faeb8defdad3ad175f1f24b344947981f9bfad54
petarkosic <avt3914@gmail.com>
2023-07-04 23:55:04 +0200

    ADD - card style changes
    Add card wrapper and container.
    Add credit card style (premium or regular).

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -71,21 +71,24 @@ function Client() {
             {data?.map(client => (
                 <motion.div
                     key={client.client_id}
-                    className='client-card'
+                    className='client-card--wrapper'
                     initial={{ opacity: 0, scaleY: 0 }}
                     animate={{ opacity: 1, scaleY: 1 }}
                     exit={{ opacity: 0, scaleY: 0 }}
                     transition={{ duration: .1 }}
                 >
-                    <div className='client-info'>
-                        <div className="client-fullname">
-                            <h1>{client.first_name}{' '}{client.last_name}</h1>
-                            <span className={typeOfCustomer}></span>
-                        </div>
-                        <p className='account-number'>{client.account_number}</p>
-                        <div className="type">
-                            <p>{client.type_of_account}</p>
-                            <p>{client.type_of_customer}</p>
+                    <div className="client-card--container">
+                        <div className='client-info'>
+                            <div className={`credit-card--info ${typeOfCustomer}`}>
+                                <div className="client-fullname">
+                                    <p className='account-number'>{client.account_number}</p>
+                                    <h1>{client.first_name}{' '}{client.last_name}</h1>
+                                </div>
+                                <div className="type">
+                                    <p>{client.type_of_account}</p>
+                                    <p>{client.type_of_customer}</p>
+                                </div>
+                            </div>
                             <button className="button-switch" onClick={() => setOpenCustomerModal(true)}>Switch to {typeOfCustomer === 'premium' ? 'regular' : 'premium'}</button>
                             {openCustomerModal &&
                                 <SwitchCustomerModal
@@ -94,52 +97,52 @@ function Client() {
                                     clientId={clientId}
                                 />
                             }
-                        </div>
-                        <div className="address">
-                            <p>{client.street_name}</p>
-                            <p>{client.house_number}</p>
-                            <p>{client.postal_code}</p>
-                            <p>{client.country_name}</p>
-                            <p>({client.country_code})</p>
-                            <button className="address-button" onClick={() => setOpenAddressModal(true)}>Change Address</button>
-                            {openAddressModal &&
-                                <EditAddressModal
-                                    setOpenAddressModal={setOpenAddressModal}
-                                    data={data}
-                                />}
-                        </div>
-                    </div>
-                    <div className="client-money">
-                        <p>Card limit: {client.card_limit}</p>
-                        <p>Credit payment: {client.credit_payment}</p>
-                        <div className="money-deposit">
-                            <p>Deposited amount: {client.deposited_amount?.slice(0, -2)}</p>
-                            <button
-                                ref={depositWithdrawButtonRef}
-                                onClick={e => handleShowInput(e)}
-                            >
-                                Deposit or Withdraw
-                            </button>
-                            <div className="input-amount">
-                                <input
-                                    type="text"
-                                    ref={depositWithdrawInputRef}
-                                    onChange={e => setChangeAmount(e.target.value)}
-                                    style={{ display: 'none' }}
-                                    placeholder='Amount' />
-                                <button
-                                    type='submit'
-                                    ref={depositWithdrawSubmitButton}
-                                    style={{ display: 'none' }}
-                                    className="submit-button"
-                                    onClick={e => handleSubmit(e)}
-                                >
-                                    Submit
-                                </button>
+                            <div className="address">
+                                <p>{client.street_name}</p>
+                                <p>{client.house_number}</p>
+                                <p>{client.postal_code}</p>
+                                <p>{client.country_name}</p>
+                                <p>({client.country_code})</p>
+                                <button className="address-button" onClick={() => setOpenAddressModal(true)}>Change Address</button>
+                                {openAddressModal &&
+                                    <EditAddressModal
+                                        setOpenAddressModal={setOpenAddressModal}
+                                        data={data}
+                                    />}
                             </div>
                         </div>
-                        <p>Currency name: {client.currency_name}</p>
-                        <p>Currency code: {client.currency_code}</p>
+                        <div className="client-money">
+                            <p>Card limit: {client.card_limit}</p>
+                            <p>Credit payment: {client.credit_payment}</p>
+                            <div className="money-deposit">
+                                <p>Deposited amount: {client.deposited_amount?.slice(0, -2)}</p>
+                                <button
+                                    ref={depositWithdrawButtonRef}
+                                    onClick={e => handleShowInput(e)}
+                                >
+                                    Deposit or Withdraw
+                                </button>
+                                <div className="input-amount">
+                                    <input
+                                        type="text"
+                                        ref={depositWithdrawInputRef}
+                                        onChange={e => setChangeAmount(e.target.value)}
+                                        style={{ display: 'none' }}
+                                        placeholder='Amount' />
+                                    <button
+                                        type='submit'
+                                        ref={depositWithdrawSubmitButton}
+                                        style={{ display: 'none' }}
+                                        className="submit-button"
+                                        onClick={e => handleSubmit(e)}
+                                    >
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                            <p>Currency name: {client.currency_name}</p>
+                            <p>Currency code: {client.currency_code}</p>
+                        </div>
                     </div>
                 </motion.div>
             ))

4ea198ed477755822ed71ab1f6df1ab2d13fe739
petarkosic <avt3914@gmail.com>
2023-11-04 23:50:08 +0100

    FIX - protected routes
    Add protected route component.
    Wrap all routes with protected route.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import Clients from './components/Clients';
 import Client from './components/Client';
@@ -7,44 +7,31 @@ import Header from './components/Header';
 import CreateClient from './components/CreateClient';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
-import { useEffect } from 'react';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   const location = useLocation();
   const isUserLoggedIn = !!localStorage.getItem('accessToken');
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isUserLoggedIn) {
-      return navigate('/login');
-    }
-  }, [isUserLoggedIn, navigate])
-
 
   return (
     <div className='wrapper'>
       <div className='container'>
-        {!isUserLoggedIn && (
-          <Routes>
+        {isUserLoggedIn && <Header />}
+        <AnimatePresence
+          mode='wait'
+          initial={false}
+        >
+          <Routes key={location.pathname} location={location}>
+            <Route element={<ProtectedRoute />} >
+              <Route path='/' element={<Clients />} />
+              <Route path='/:clientId' element={<Client />} />
+              <Route path='/create-client' element={<CreateClient />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
+
             <Route path='/login' element={<Login />} />
           </Routes>
-        )}
-        {isUserLoggedIn && (
-          <>
-            <Header />
-            <AnimatePresence
-              mode='wait'
-              initial={false}
-            >
-              <Routes key={location.pathname} location={location}>
-                <Route path='/' element={<Clients />} />
-                <Route path='/:clientId' element={<Client />} />
-                <Route path='/create-client' element={<CreateClient />} />
-                <Route path='/dashboard' element={<Dashboard />} />
-              </Routes>
-            </AnimatePresence>
-          </>
-        )}
+        </AnimatePresence>
       </div>
     </div>
   );

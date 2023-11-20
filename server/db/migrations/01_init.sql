19deb2775cbf31725e9ec5040bd5767f067743cd
petarkosic <avt3914@gmail.com>
2023-11-20 23:57:13 +0100

    FIX - move tellers table to init script
    Move tellers table creation to 01_init.sql script so that both dev and prod environments would have it.

--- server/db/migrations/01_init.sql
+++ server/db/migrations/01_init.sql
@@ -91,3 +91,29 @@ CREATE TRIGGER trigger_update_withdrawal_fee
     FOR EACH ROW
     EXECUTE FUNCTION update_withdrawal_fee();
 
+
+
+CREATE TABLE tellers(
+    teller_id SERIAL,
+    login_id VARCHAR(7) NOT NULL UNIQUE,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    registered_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    PRIMARY KEY (teller_id)
+);
+
+CREATE OR REPLACE FUNCTION generate_login_id() 
+RETURNS VARCHAR(7) AS $$
+DECLARE
+  chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+  result text := '';
+  i integer := 0;
+BEGIN
+  FOR i IN 1..7 LOOP
+    result := result || chars[ceil(61 * random())];
+  END LOOP;
+  RETURN result;
+END;
+$$ LANGUAGE plpgsql;
